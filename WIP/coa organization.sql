a499b602d02381df7f9d4ebdd412f18659f3ab3b
lichfiet <143028846+lichfiet@users.noreply.github.com>
2023-09-11 10:58:19 -0600

    Update coa organization.sql

--- WIP/coa organization.sql
+++ WIP/coa organization.sql
@@ -1,18 +1,38 @@
+-- sets accounts to new department and code and acctdept, based on if they had an acctcode including a deptcode
 UPDATE glchartofaccounts coa
-SET departmentid = meow.newdepartmentid,
-	acctdept = meow.newdept
+SET acctcode = meow.newacctcode,
+	acctdept = meow.newacctdept,
+	deptid = meow.newdepartmentid
 FROM (
-	SELECT coa.acctdeptid AS glid,
-		coa.acctdept,
-		('-' || newd.deptcode) AS newdepartment,
+	SELECT CASE 
+			WHEN RIGHT(coa.acctcode, 3) = ('-' || newd.deptcode)
+				THEN LEFT(coa.acctcode, (LENGTH(coa.acctcode) - 3))
+			ELSE acctcode
+			END AS newacctcode,
+		CASE 
+			WHEN RIGHT(coa.acctcode, 3) = ('-' || newd.deptcode)
+				THEN LEFT(coa.acctcode, (LENGTH(coa.acctcode) - 3)) || ('-' || newd.deptcode)
+			ELSE acctcode || ('-' || newd.deptcode)
+			END AS newacctdept,
+		coa.acctdeptid AS acctid,
+		coa.acctdept AS currdept,
+		coa.acctcode AS currcode,
 		newd.departmentsid AS newdepartmentid,
-		(RIGHT(coa.acctdept, 3)) AS currdepartment,
-		LEFT(coa.acctdept, (LENGTH(acctdept) - 3)) AS newdept
+		coa.deptid AS currdepartmentid,
+		'-' || d.deptcode AS currdeptcode,
+		('-' || newd.deptcode) AS newdepartment
 	FROM glchartofaccounts coa
+	-- join to find new dept code
+	INNER JOIN gldepartment d ON d.departmentsid = coa.deptid
 	INNER JOIN gldepartment newd ON ('-' || newd.deptcode) = (RIGHT(coa.acctdept, 3))
 		AND newd.accountingid = coa.accountingid
+	--	AND newd.departmentsid != coa.deptid
+	-- join to find new sequence number
+	ORDER BY ('-' || newd.deptcode),
+		coa.acctdept
 	) meow
-WHERE meow.glid = coa.acctdeptid;
+WHERE meow.acctid = coa.acctdeptid
+
 
 SELECT row_Number() OVER (
 		PARTITION BY deptid ORDER BY coa.acctdept
