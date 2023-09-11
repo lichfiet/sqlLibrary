cc6df617a91d736255fba4f584740549282cc364
lichfiet <143028846+lichfiet@users.noreply.github.com>
2023-09-11 13:22:21 -0600

    Update coa organization.sql

--- WIP/coa organization.sql
+++ WIP/coa organization.sql
@@ -34,35 +34,42 @@ FROM (
 WHERE meow.acctid = coa.acctdeptid
 
 -- reorganizes department accounts
-SELECT row_Number() OVER (
-		PARTITION BY d.deptcode ORDER BY regexp_replace(coa.acctdept, '[^0-9]+', '', 'g')
-		) + (d.deptcode::int * 10000),
-	CASE 
-		WHEN textregexeq(coa.acctdept, '^[[:digit:]]+(\.[[:digit:]]+)?$') = true
-			THEN 0
-		ELSE 1
-		END,
-	d.deptcode,
-	regexp_replace(coa.acctdept, '[^0-9]+', '', 'g'),
-	textregexeq(coa.acctdept, '^[[:digit:]]+(\.[[:digit:]]+)?$') AS isnumeric,
-	CASE 
-		WHEN d.deptcode = ''
-			THEN 0
-		ELSE cast(d.deptcode AS INT)
-		END AS test,
-	*
-FROM glchartofaccounts coa
-INNER JOIN (
-	SELECT acctdeptid,
-		deptcode
-	FROM glchartofaccounts
-	INNER JOIN gldepartment ON deptid = departmentsid
-	) row ON row.acctdeptid = coa.acctdeptid
-INNER JOIN gldepartment d ON d.departmentsid = coa.deptid
-WHERE d.deptcode != ''
-ORDER BY d.deptcode,
+UPDATE glchartofaccounts coa
+SET sequencenumber = bob.epicsequencenumber
+FROM (
+	SELECT coa.acctdeptid as id,
 	row_Number() OVER (
-		PARTITION BY d.deptcode ORDER BY regexp_replace(coa.acctdept, '[^0-9]+', '', 'g')
-		)
+			PARTITION BY d.deptcode ORDER BY regexp_replace(coa.acctdept, '[^0-9]+', '', 'g')
+			) + (d.deptcode::INT * 1000000) AS epicsequencenumber,
+		CASE 
+			WHEN textregexeq(coa.acctdept, '^[[:digit:]]+(\.[[:digit:]]+)?$') = true
+				THEN 0
+			ELSE 1
+			END,
+		d.deptcode,
+		regexp_replace(coa.acctdept, '[^0-9]+', '', 'g'),
+		textregexeq(coa.acctdept, '^[[:digit:]]+(\.[[:digit:]]+)?$') AS isnumeric,
+		CASE 
+			WHEN d.deptcode = ''
+				THEN 0
+			ELSE cast(d.deptcode AS INT)
+			END AS test,
+		*
+	FROM glchartofaccounts coa
+	INNER JOIN (
+		SELECT acctdeptid,
+			deptcode
+		FROM glchartofaccounts
+		INNER JOIN gldepartment ON deptid = departmentsid
+		) row ON row.acctdeptid = coa.acctdeptid
+	INNER JOIN gldepartment d ON d.departmentsid = coa.deptid
+	WHERE d.deptcode != ''
+	ORDER BY d.deptcode,
+		row_Number() OVER (
+			PARTITION BY d.deptcode ORDER BY regexp_replace(coa.acctdept, '[^0-9]+', '', 'g')
+			)
+	) bob
+WHERE bob.id = coa.acctdeptid
+
 
 
