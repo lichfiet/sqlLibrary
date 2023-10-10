0bfe2d0b1c6003be74d8dfd0a7ed0ac439fa740c
lichfiet <143028846+lichfiet@users.noreply.github.com>
2023-10-10 16:54:00 -0600

    Update unhideadmin.sql

--- unhideadmin.sql
+++ unhideadmin.sql
@@ -1,7 +1,7 @@
 -- Unhides your admin login so you can enable them to be a cashier
 --
 -- SQL Description: updates storeid and hidden status in coprincipal
--- How to Use: replace username on line 19 with your username, keep surround in % symbols
+-- How to Use: replace username on line 19 with your username, keep surround in % symbols, then make sure you are logged in to the DB before you run the SQL
 -- Jira Key/CR Number: N/A
 -- SQL Statement:
 
