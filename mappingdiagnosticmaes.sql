617b0bcaae7c177a152f3ddc5e683464019cb643
lichfiet <143028846+lichfiet@users.noreply.github.com>
2023-10-13 22:45:31 -0600

    Update mappingdiagnosticmaes.sql

--- mappingdiagnosticmaes.sql
+++ mappingdiagnosticmaes.sql
@@ -1,3 +1,41 @@
+-- finished barcode update
+WITH barcodeupdateoffset
+AS (
+	SELECT CASE 
+			WHEN value = '0'
+				OR coa.acctdeptid IS NULL
+				THEN 0
+			ELSE 1
+			END AS mapstate,
+		storeid
+	FROM copreference
+	LEFT JOIN glchartofaccounts coa ON coa.acctdeptid::VARCHAR = value
+	WHERE id = 'shop-InventoryPartUpdateOffsettingGLAccount'
+		AND value = '0'
+	),
+warrcompanymapping
+AS (
+SELECT * FROM cowarrantycompany
+)
+SELECT bai.description,
+	CASE 
+		WHEN documentitemtype != 10060002
+			THEN ''
+		WHEN barcodeupdateoffset.mapstate = 1
+			AND documentitemtype != 10060002
+			THEN 'Mapped'
+		WHEN barcodeupdateoffset.mapstate = 0
+			AND documentitemtype != 10060002
+			THEN '(System > Preferences > Parts Folder > Inventory Updating > Offset GL Account)'
+		END AS barcodeupdate,
+	*
+FROM mabusinessactionitem bai
+INNER JOIN mabusinessaction ba ON ba.businessactionid = bai.businessactionid
+INNER JOIN barcodeupdateoffset ON barcodeupdateoffset.storeid = bai.storeid
+WHERE ba.STATUS = 2;
+
+
+
 -- for warranty
 SELECT CASE 
 		WHEN warrupdateAP.vendorid IS NOT NULL
