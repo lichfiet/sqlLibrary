b9e23a8a08c14795cd472314325073177a00ec8f
lichfiet <143028846+lichfiet@users.noreply.github.com>
2023-10-28 13:50:44 -0600

    Update payrollPlusTool.sql

--- payrollPlusTool.sql
+++ payrollPlusTool.sql
@@ -9,9 +9,12 @@
 SELECT co.storeid AS StoreID,
 	co2.value AS CMF,
 	co3.value AS LocationName,
+	s.storecode AS StoreCode,
+	s.storename AS StoreName,
 	co.value AS PayrollType,
 	co4.value AS PayrollPlus_ClientID
 FROM copreference co
+INNER JOIN costore s on s.storeid = co.storeid
 INNER JOIN copreference co2 ON co2.storeidluid = co.storeidluid
 INNER JOIN copreference co3 ON co3.storeidluid = co.storeidluid
 INNER JOIN copreference co4 ON co4.storeidluid = co.storeidluid
