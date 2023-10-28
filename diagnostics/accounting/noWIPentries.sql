4839af51877573b8bcbfedb0c7257788f3d447ec
lichfiet <143028846+lichfiet@users.noreply.github.com>
2023-10-28 09:23:14 -0600

    Update noWIPentries.sql

--- diagnostics/accounting/noWIPentries.sql
+++ diagnostics/accounting/noWIPentries.sql
@@ -5,9 +5,9 @@
 -- How to Use: Copy the SQL statement, paste it in phoenix, and click run, no modification is neccesary.
 -- Jira Key/CR Number: N/A
 -- SQL Statement:
-
 -- To find affected repair orders that are open
-SELECT ro.repairordernumber, ro.dtstamp
+SELECT ro.repairordernumber,
+	ro.dtstamp
 FROM selaborsession ls
 INNER JOIN serepairorderlabor rol ON ls.repairorderlaborid = rol.repairorderlaborid
 INNER JOIN serepairorderjob roj ON roj.repairorderjobid = rol.repairorderjobid
@@ -16,8 +16,13 @@ INNER JOIN serepairorder ro ON ro.repairorderid = rou.repairorderid
 INNER JOIN setechnician t ON t.technicianid = ls.technicianid
 	AND t.technicianrate != ls.technicianrate
 WHERE STATE NOT IN (2, 3, 0) -- remove the 2 to see cashiered ROs
-	AND ls.technicianrate = 0
-	AND ro.storeid = 2
+	AND (
+		ls.technicianrate = 0
+		OR (
+			ls.creditedhours = 0
+			AND ls.actualhours = 0
+			)
+		)
 GROUP BY ro.repairordernumber,
 	ro.dtstamp
 ORDER BY ro.dtstamp DESC;
@@ -36,6 +41,11 @@ INNER JOIN serepairorder ro ON ro.repairorderid = rou.repairorderid
 INNER JOIN setechnician t ON t.technicianid = ls.technicianid
 	AND t.technicianrate != ls.technicianrate
 WHERE STATE NOT IN (2, 3, 0) -- remove the 2 to see casiered ROs
-	AND ls.technicianrate = 0
+	AND (
+		ls.technicianrate = 0
+		OR (
+			ls.creditedhours = 0
+			AND ls.actualhours = 0
+			)
+		)
 ORDER BY ro.dtstamp DESC;
-
