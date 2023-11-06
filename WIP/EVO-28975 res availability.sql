049e124fa47c0b9190b0ed17d898dcbb230b6aaf
lichfiet <143028846+lichfiet@users.noreply.github.com>
2023-11-06 10:18:27 -0700

    Update EVO-28975 res availability.sql

--- WIP/EVO-28975 res availability.sql
+++ WIP/EVO-28975 res availability.sql
@@ -48,6 +48,7 @@ AS (
 	--
 	FROM rerentalitem ri
 	INNER JOIN rereservationitem resi ON resi.rentalitemid = ri.rentalitemid
+	WHERE STATE != 5
 	),
 problemrentals
 	/* This CTE is used to compare the start and end time of rentals based on their end date, and their start dates to gauge whether there will be overlapping availability.
@@ -70,6 +71,7 @@ AS (
 	SELECT ri.rentalitemnumber AS curritemnumber,
 		ri.rentaltypeid,
 		ri.rentalitemid,
+		ri.storeid,
 		Array [rds.reservationid, rde.reservationid] AS reservationids, -- res item ids
 		-- begin ultimate case whens
 		CASE 
@@ -78,7 +80,7 @@ AS (
 					rde.contractstart < rds.contractend
 					AND rde.contractstart != rds.contractstart
 					AND rde.STATE = 2 -- next res ongoing
-					AND rds.STATE NOT IN (1, 2) -- previous is stopped
+					AND rds.STATE != 2 -- previous is stopped
 					)
 				THEN ARRAY [rde.contractstart, rde.contractend, 'current res starts before previous res end', 'Current res start overlap, previous stopped', 'test1']
 			WHEN (
@@ -103,7 +105,7 @@ AS (
 					-- opt4
 					rds.contractend > rde.contractstart
 					AND rds.STATE = 2
-					AND rde.STATE NOT IN (1, 2)
+					AND rde.STATE != 2
 					)
 				THEN ARRAY [rds.contractstart, rds.contractend, 'previous reservation ends after the start of the current res and current is stopped', 'Previous res ends after Current, Current is stopped', 'test4']
 			END AS textfields,
@@ -115,9 +117,9 @@ AS (
 					rde.contractstart < rds.contractend
 					AND rde.contractstart != rds.contractstart
 					AND rde.STATE = 2 -- next res ongoing
-					AND rds.STATE NOT IN (1, 2) -- previous is stopped
+					AND rds.STATE != 2 -- previous is stopped
 					)
-				THEN ARRAY [rde.reservationitemid]
+				THEN ARRAY [rde.reservationitemid, rde.reservationid]
 			WHEN (
 					rds.STATE = 2 -- previous res ongoing
 					AND rde.STATE = 2 -- current res ongoing
@@ -125,7 +127,7 @@ AS (
 					AND rde.contractstart != rds.contractstart
 					AND rds.noenddate = 1 -- previous res has no end date
 					)
-				THEN ARRAY [rds.reservationitemid]
+				THEN ARRAY [rds.reservationitemid, rds.reservationid]
 			WHEN (
 					rde.contractstart = rds.contractstart
 					AND (
@@ -133,13 +135,25 @@ AS (
 						OR rde.STATE = 2
 						)
 					)
-				THEN ARRAY [rde.reservationitemid, CASE when rde.state = 2 and rds.state != 2 then rde.reservationid when rds.state = 2 and rde.state != 2 then rds.reservationid WHEN rds.state = 2 and rde.state = 2 then rds.reservationid END]
+				THEN (
+						CASE 
+							WHEN rde.STATE = 2
+								AND rds.STATE != 2
+								THEN ARRAY [rde.reservationitemid, rde.reservationid]
+							WHEN rds.STATE = 2
+								AND rde.STATE != 2
+								THEN ARRAY [rds.reservationitemid, rds.reservationid]
+							WHEN rds.STATE = 2
+								AND rde.STATE = 2
+								THEN ARRAY [rds.reservationitemid, rds.reservationid]
+							END
+						)
 			WHEN (
 					rds.contractend > rde.contractstart
 					AND rds.STATE = 2
-					AND rde.STATE NOT IN (1, 2)
+					AND rde.STATE != 2
 					)
-				THEN ARRAY [rds.reservationitemid]
+				THEN ARRAY [rds.reservationitemid, rds.reservationid]
 			END AS intfields
 	FROM rerentalitem ri
 	INNER JOIN reservationdates rds ON rds.itemid = ri.rentalitemid
@@ -154,7 +168,7 @@ AS (
 			rde.contractstart < rds.contractend
 			AND rde.contractstart != rds.contractstart
 			AND rde.STATE = 2 -- next res ongoing
-			AND rds.STATE NOT IN (1, 2) -- previous is stopped
+			AND rds.STATE != 2 -- previous is stopped
 			)
 		OR (
 			-- Where both reservations are ongoing, and the current one was started after a previous reservation with no end date (Sometimes fixable on front-end)
@@ -179,11 +193,11 @@ AS (
 			--
 			rds.contractend > rde.contractstart
 			AND rds.STATE = 2
-			AND rde.STATE NOT IN (1, 2)
+			AND rde.STATE != 2
 			)
 	),
 availablerentals
-/* This case when is used to identify rental items, that do not have a schedule reservation within the time-frame of one of the reservations with issues
+	/* This case when is used to identify rental items, that do not have a schedule reservation within the time-frame of one of the reservations with issues
 this is usually either the only reservation that is open of the two, or the previous reservation. Items that have no history at all will be populated*/
 AS (
 	SELECT ri.rentalitemnumber AS newitemnumber,
@@ -207,7 +221,8 @@ AS (
 			END AS availend,
 		rds.contractstart || ' --> ' || rds.contractend || ' || ' || rde.contractstart || ' --> ' || rde.contractend,
 		rds.STATE AS curritemstate,
-		rde.STATE AS nextritemstate
+		rde.STATE AS nextritemstate,
+		ri.storeid
 	FROM rerentalitem ri
 	INNER JOIN reservationdates rds ON rds.itemid = ri.rentalitemid
 	LEFT JOIN reservationdates rde ON rde.itemid = rds.itemid
@@ -223,7 +238,8 @@ AS (
 		'30000-09-30' AS availend,
 		'10000-09-30' || ' --> ' || '10000-09-30' || ' || ' || '30000-09-30' || ' --> ' || '30000-09-30',
 		1 AS curritemstate,
-		1 AS nextritemstate
+		1 AS nextritemstate,
+		ri.storeid
 	FROM rerentalitem ri
 	LEFT JOIN rereservationitem rei ON rei.rentalitemid = ri.rentalitemid
 	WHERE rei.reservationitemid IS NULL
@@ -249,8 +265,9 @@ AS (
 		ar.newitemnumber AS newitem_number,
 		ar.availstart,
 		ar.availend,
-		ar.rentalitemid AS newitemid,
-		pr.intfields [1] AS reservationitemid,
+		ar.rentalitemid AS new_itemid,
+		'Update Res #' || changeres.reservationnumber AS res_num_to_modify,
+		pr.intfields [1] AS reservationitemid_to_modify,
 		row_Number() OVER (
 			PARTITION BY pr.rentalitemid ORDER BY CASE 
 					WHEN ar.rentaltypeid = pr.rentaltypeid
@@ -265,8 +282,10 @@ AS (
 	FROM problemrentals pr
 	INNER JOIN rereservation rdsr ON pr.reservationids [1] = rdsr.reservationid
 	LEFT JOIN rereservation rder ON pr.reservationids [2] = rder.reservationid
+	LEFT JOIN rereservation changeres ON pr.intfields [2] = changeres.reservationid
 	LEFT JOIN availablerentals ar ON pr.textfields [1] > ar.availstart
 		AND ar.availend >= pr.textfields [2]
+		AND ar.storeid = pr.storeid
 	ORDER BY pr.curritemnumber ASC
 	)
 SELECT *
