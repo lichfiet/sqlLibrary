d38bbd97001f84f5ac481fdd725af2a8478620d8
lichfiet <143028846+lichfiet@users.noreply.github.com>
2023-11-24 00:42:00 -0700

    Update EVO-20828 Update(s).sql

--- Management Activities/Management Activity Errors/Parts/EVO-20828 Update(s).sql
+++ Management Activities/Management Activity Errors/Parts/EVO-20828 Update(s).sql
@@ -48,23 +48,87 @@ FROM (
 WHERE bob.partinvoicelineid = pil.partinvoicelineid;
 
 
--- riley's
 
--- EVO-20828
--- Invoice xxxxx
-UPDATE papartinvoiceline
-SET adjustmentprice = adjustmentprice - data.dif
+-- EXPERIMENTAL UPDATE WITH DIAG, Need to add a join to eliminate non-pay part invoices because they show the same output with the diag
+UPDATE papartinvoiceline pil
+SET adjustmentprice = adjustmentprice + bob.adjamt
 FROM (
-	SELECT pit.soldnowsubtotal AS true,
-		pi.invoicecollectamt AS false,
-		pi.partinvoiceid AS piid,
-		pil.partinvoicelineid as pilid,
-		(pit.soldnowsubtotal - pi.invoicecollectamt) AS dif,
-		pil.adjustmentprice as incorrectamount,
-		(pil.adjustmentprice - (pit.soldnowsubtotal - pi.invoicecollectamt)) as correctamount
-	FROM papartinvoice pi
-	INNER JOIN papartinvoicetotals pit ON pi.partinvoiceid = pit.partinvoiceid
-	Inner join papartinvoiceline pil on pi.partinvoiceid = pil.partinvoiceid
-	WHERE pil.partinvoicelineid = xxxxxxx
-	) data
-WHERE partinvoicelineid = data.pilid;
+	SELECT partinvoicelineid,
+		row_Number() OVER (
+			PARTITION BY partinvoiceid ORDER BY qtysold DESC,
+				qtyspecialorder DESC
+			),
+		sum(debitamt) OVER (PARTITION BY partinvoiceid) AS debits,
+		sum(creditamt) OVER (PARTITION BY partinvoiceid) AS credits,
+		sum(debitamt - creditamt) OVER (PARTITION BY partinvoiceid) AS OOB_Amount,
+		(
+			Round(sum(debitamt - creditamt) OVER (PARTITION BY partinvoiceid) / (
+					CASE 
+						WHEN qtysold != 0
+							THEN qtysold
+						ELSE qtyspecialorder
+						END
+					), 4) * 10000
+			)::INTEGER AS adjamt
+	FROM papartinvoiceline pil
+	INNER JOIN mabusinessaction ba ON ba.documentid = pil.partinvoiceid
+	INNER JOIN mabusinessactionitem bai ON bai.businessactionid = ba.businessactionid
+	INNER JOIN (
+		SELECT businessactionid
+		FROM (
+			SELECT ba.businessactionid
+			FROM papartinvoiceline pi
+			INNER JOIN papartinvoicetotals pit ON pit.partinvoiceid = pi.partinvoiceid
+			INNER JOIN papartinvoicetaxitem piti ON piti.partinvoiceid = pi.partinvoiceid
+			INNER JOIN papartinvoicetaxentity pite ON pite.partinvoicetaxitemid = piti.partinvoicetaxitemid
+			INNER JOIN mabusinessaction ba ON ba.documentid = pi.partinvoiceid
+			INNER JOIN (
+				SELECT pi.partinvoiceid
+				FROM papartinvoice pi
+				INNER JOIN papartinvoicetotals pit ON pit.partinvoiceid = pi.partinvoiceid
+				INNER JOIN (
+					SELECT SUM((qtyspecialorder * adjustmentprice) / 10000) AS amt,
+						partinvoiceid
+					FROM papartinvoiceline
+					GROUP BY partinvoiceid
+					) soamt ON soamt.partinvoiceid = pi.partinvoiceid
+				WHERE pi.specialordercollectamount = (soamt.amt + pit.specialordertax)
+				) v1 ON v1.partinvoiceid = pi.partinvoiceid
+			INNER JOIN (
+				SELECT SUM(debitamt - creditamt) AS oob,
+					bai.businessactionid
+				FROM mabusinessactionitem bai
+				INNER JOIN mabusinessaction ba ON ba.businessactionid = bai.businessactionid
+				WHERE ba.documenttype = 1001
+					AND ba.STATUS = 2
+				GROUP BY bai.businessactionid
+				) oob ON oob.businessactionid = ba.businessactionid
+			INNER JOIN (
+				SELECT SUM(depositapplied) AS applied,
+					partinvoiceid
+				FROM papartinvoiceline
+				GROUP BY partinvoiceid
+				) dep ON dep.partinvoiceid = pi.partinvoiceid
+			INNER JOIN (
+				SELECT pi.partinvoiceid
+				FROM papartinvoice pi
+				INNER JOIN papartinvoicetotals pit ON pit.partinvoiceid = pi.partinvoiceid
+				INNER JOIN (
+					SELECT sum((qtyspecialorder * adjustmentprice) / 10000) AS amt,
+						partinvoiceid
+					FROM papartinvoiceline
+					GROUP BY partinvoiceid
+					) soamt ON soamt.partinvoiceid = pi.partinvoiceid
+					AND pi.specialordercollectamount = (soamt.amt + pit.specialordertax)
+				) soa ON soa.partinvoiceid = pi.partinvoiceid
+			WHERE ba.documenttype = 1001
+				AND ba.STATUS = 2
+				AND dep.applied <> oob.oob
+			GROUP BY ba.businessactionid
+			) data
+		GROUP BY businessactionid
+		) diag ON diag.businessactionid = ba.businessactionid
+	WHERE ba.STATUS = 2
+	) bob
+WHERE bob.partinvoicelineid = pil.partinvoicelineid
+	AND row_number = 1;
