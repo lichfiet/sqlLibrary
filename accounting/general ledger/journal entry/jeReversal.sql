71b57d5112e203c8bdad914bea0a1d64fdaab792
Trevor Lichfield <143028846+lichfiet@users.noreply.github.com>
2023-12-27 13:54:49 -0700

    Create jeReversal.sql

---
+++ accounting/general ledger/journal entry/jeReversal.sql
@@ -0,0 +1,14 @@
+SELECT h.documentnumber,
+	left(h.DATE::VARCHAR, 9) AS DATE,
+	coa.acctdept AS accountnumber,
+	ROUND(amtcredit * .0001, 2) AS credit,
+	ROUND(amtdebit * .0001, 2) AS debit,
+	description || ' - Reversal' AS description1,
+	0 AS description2,
+	'reverse entry' AS ref1,
+	0 AS ref2,
+	h.locationid
+FROM glhistory h
+INNER JOIN glchartofaccounts coa ON coa.acctdeptid = h.acctdeptid
+WHERE journalentryid IN --(2336952, 2336953, 2336954, 2336955, 2336956, 2336957, 2336958, 2336959, 2336960, 2336961, 2336962, 2336963, 2336964, 2336965, 2336966, 2336967)
+ORDER BY documentnumber
