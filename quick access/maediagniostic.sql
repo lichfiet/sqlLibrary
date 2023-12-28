0b567f19bb86f8e52fcf3fb1ad6b5f870b6e9293
Trevor Lichfield <143028846+lichfiet@users.noreply.github.com>
2023-12-28 14:17:20 -0700

    Update maediagniostic.sql

--- quick access/maediagniostic.sql
+++ quick access/maediagniostic.sql
@@ -520,6 +520,7 @@ AS (
 	INNER JOIN mabusinessaction ba ON ba.documentid = h.partinvoiceid
 	WHERE il.partinvoiceid <> h.partinvoiceid
 		AND ba.STATUS = 2
+	GROUP BY ba.businessactionid
 	),
 oobmissingdiscountpartinvoice
 AS (
