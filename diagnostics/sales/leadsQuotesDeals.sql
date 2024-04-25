4ebea6c6f7a5b0796203b8862c396bc50e1875e1
Trevor Lichfield <sortaminty@gmail.com>
2024-04-24 23:40:47 -0600

    Update leadsQuotesDeals.sql

--- diagnostics/sales/leadsQuotesDeals.sql
+++ diagnostics/sales/leadsQuotesDeals.sql
@@ -1,6 +1,6 @@
 WITH search
 AS (
-    --
+	--
 	/* Replace CHANGE ME with the deal number you want to run the diagnostic for */
 	--
 	SELECT 'CHANGE ME' AS dealnumber
@@ -21,7 +21,7 @@ AS (
 	INNER JOIN costore s ON s.storeid = co.storeid
 	INNER JOIN costoremap sm ON sm.childstoreid = s.storeid
 	INNER JOIN copreference co2 ON co2.storeidluid = co.storeidluid
-	INNER JOIN search ON search.dealnumber = d.dealnumber::varchar
+	INNER JOIN search ON search.dealnumber = d.dealnumber::VARCHAR
 	WHERE co.id = 'shop-DealerID'
 		AND co2.id = 'shop-LocationName'
 	ORDER BY co2.value ASC
@@ -39,11 +39,25 @@ AS (
 	WHERE muo.isreversed = 1
 		AND mup.dtstamp > '2019-05-01'
 	GROUP BY du.dealid
+	),
+evo30652
+AS (
+	SELECT du.dealid,
+	    'EVO-26439 or EVO-27350 - Could not locate a RepairOrderID Error When Finalizinng | EVO-27350 is T1 Preapproved, if it does not work T2' AS description
+	FROM samajorunitlabor mul
+	LEFT JOIN serepairorderlabor rol ON rol.repairorderlaborid = mul.repairorderlaborid
+	INNER JOIN samajorunit mu ON mu.majorunitid = mul.majorunitid
+	INNER JOIN sadealunit du ON du.majorunitid = mu.majorunitid
+	INNER JOIN dealinfo di ON di.dealid = du.dealid
+	WHERE rol.repairorderlaborid IS NULL
+		AND mul.STATE IN (2)
+	GROUP BY du.dealid
 	)
 SELECT 'CMF#: ' || di.cmf AS cmf_number,
 	'Location: ' || di.storename AS location_name,
 	'Deal #: (' || di.deal_number || ')',
-	coalesce(evo26439.description, 'No CRs Identified On Deal') AS problem_description_and_cr,
+	coalesce(evo26439.description, evo30652.description, 'No CRs Identified On Deal') AS problem_description_and_cr,
 	di.storeid
 FROM dealinfo di
-LEFT JOIN evo26439 ON evo26439.dealid = di.dealid;
+LEFT JOIN evo26439 ON evo26439.dealid = di.dealid
+LEFT JOIN evo30652 ON evo30652.dealid = di.dealid;
