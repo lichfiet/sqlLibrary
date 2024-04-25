0faab6405839126187cec54d696fc49854931615
Trevor Lichfield <sortaminty@gmail.com>
2024-04-24 22:49:57 -0600

    Update jeReversal.sql

--- tools/jeReversal.sql
+++ tools/jeReversal.sql
@@ -12,3 +12,22 @@ FROM glhistory h
 INNER JOIN glchartofaccounts coa ON coa.acctdeptid = h.acctdeptid
 WHERE journalentryid IN --(2336952, 2336953, 2336954, 2336955, 2336956, 2336957, 2336958, 2336959, 2336960, 2336961, 2336962, 2336963, 2336964, 2336965, 2336966, 2336967)
 ORDER BY documentnumber
+
+-- Alt select for MAE documents that haven't posted
+
+SELECT ba.documentnumber,
+	left(bai.dtstamp::VARCHAR, 9) AS dtstamp,
+	coa.acctdept AS accountnumber,
+	ROUND(debitamt * .0001, 2) AS debitamt,
+	ROUND(creditamt * .0001, 2) AS creditamt,
+	description || ' - Reversal' AS description1,
+	0 AS description2,
+	'reverse entry' AS ref1,
+	0 AS ref2,
+	ba.storeid
+FROM mabusinessactionitem bai
+INNER JOIN glchartofaccounts coa ON coa.acctdeptid = bai.accountid
+INNER JOIN mabusinessaction ba ON ba.businessactionid = bai.businessactionid
+WHERE ba.STATUS != 1
+	AND ba.documentnumber = 'DOCUMENT NUMBER'
+
