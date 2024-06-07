a00d6d05b05847845f6078489d8e0fdd3918f889
Trevor Lichfield <sortaminty@gmail.com>
2024-06-07 10:06:34 -0600

    Update apinvschedule.sql

--- WIP/apinvschedule.sql
+++ WIP/apinvschedule.sql
@@ -1,35 +1,42 @@
--- need to add (invoice amount = 0 if not in vendor's gl account to make it match AP Rec)
-SELECT vendornumber,
-	name,
-	documentnumber,
- 	journalentryid,
-	account_number,
-	debit, 
-	credit,
-	invoice_balance
+SELECT *
 FROM (
 	SELECT sl.sltrxid,
 		sl.documentnumber,
 		h.journalentryid,
 		coa.acctdept AS account_number,
-		ROUND(amtdebit * .0001, 2) AS debit,
-		ROUND(amtcredit * .0001, 2) AS credit,
-		ROUND(sum(amtdebit - amtcredit) OVER (PARTITION BY scheduleidentifier) * .0001, 2) AS gl_balance,
-		ROUND(sum(amtdebit - amtcredit) OVER (PARTITION BY scheduleidentifier, coa.acctdept) * .0001, 2) AS test,
-		ROUND(sl.remainingamt * .0001, 2) AS invoice_balance,
+		amtdebit AS debit,
+		amtcredit AS credit,
+		sum(amtdebit - amtcredit) OVER (PARTITION BY scheduleidentifier) AS invoice_total_gl,
+		sum(amtdebit - amtcredit) OVER (
+			PARTITION BY scheduleidentifier,
+			coa.acctdept
+			) AS invoice_total_gl_per_account,
+		sum(amtdebit - amtcredit) OVER (PARTITION BY sl.acctid) AS vendor_total_gl,
+		sum(amtdebit - amtcredit) OVER ( PARTITION BY sl.acctid, coa.acctdept ) AS vendor_total_gl_per_account,
+		CASE 
+			WHEN vt.totalvend is null
+				THEN 0
+			ELSE sl.remainingamt
+			END AS invoice_balance,
+		coalesce(vt.totalvend, 0),
+		sum(amtdebit - amtcredit) OVER ( PARTITION BY sl.acctid, coa.acctdept ) + coalesce(vt.totalvend, 0) AS rec_amt,
 		v.vendornumber,
 		v.name
 	FROM glhistory h
 	INNER JOIN glchartofaccounts coa ON coa.acctdeptid = h.acctdeptid
 	INNER JOIN glsltransaction sl ON sl.sltrxid = h.scheduleidentifier
 	INNER JOIN apvendor v ON v.vendorid = sl.acctid
-	WHERE v.vendornumber != 3241996
-		AND v.vendornumber != 687522
-		AND v.vendornumber != 2647
-		AND v.vendornumber != 32518
+	LEFT JOIN glchartofaccounts vendorgl ON vendorgl.acctdeptid = v.apglacctdeptid
+		AND v.apglacctdeptid != 0
+	LEFT JOIN (
+		SELECT sum(remainingamt) AS totalvend,
+			sl.acctid
+		FROM glsltransaction sl
+		WHERE sl.sltrxstate IN (1, 2)
+		GROUP BY sl.acctid
+		) vt ON vt.acctid = sl.acctid
+		AND vendorgl.acctdeptid is not null
+	WHERE v.vendornumber = 2403
 	) bob
-WHERE ((- 1 * invoice_balance) != gl_balance and test != 0)
-    OR (gl_balance = 0 and test = 0 and invoice_balance * -1 != 0)
+--WHERE invoice_total_gl_per_account != invoice_balance * - 1
     
-ORDER BY name, documentnumber, account_number
-
